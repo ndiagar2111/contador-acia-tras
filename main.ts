d77035d893d80ebfa8ad24918680b2a46a927e3e
ndiagar2111 <ndiagar2111@g.educaand.es>
2025-01-17 14:42:01 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     contador = 10
 })
 let contador = 0
-contador = 10
+contador = 11
 basic.forever(function () {
     contador += -1
     basic.showNumber(contador)
